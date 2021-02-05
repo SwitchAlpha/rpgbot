72de88fa460cdaa7dc575b4545000816dd501e4a
Glitch (beaded-pond-shroud) <none>
2021-02-05 22:04:21 +0000

    🐋🚡 Updated with Glitch

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,8 @@
+const { Message, DiscordAPIError } = require('discord.js');
+const db = require('quick.db');
+const Discord = require('discord.js')
+const client = new Discord.Client();
+
+module.exports = member => {
+
+};
